44a2db367b9a8e61391793ce0877de9bd165f83b
Taranator0813 <123022806+Taranator0813@users.noreply.github.com>
2023-06-15 11:24:54 -0500

    Update HtmlToPdfConverter.java

--- micro_holamundo/JAVA/HtmlToPdfConverter.java
+++ micro_holamundo/JAVA/HtmlToPdfConverter.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,30 +10,29 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class HtmlToPdfConverter {
 
     public static void main(String[] args) {
-     
+
         ChromeOptions options = new ChromeOptions();
         options.setHeadless(true);
         options.addArguments("--disable-gpu");
 
-  
         String htmlFilePath = "/home/nuevo-directorio/micro_holamundo/JMETER/ResultadosHTML/index.html";
 
-      
         String pdfOutputPath = "/home/EstefaniaM/workspace/Rollback/archivo_selenium.pdf";
 
         WebDriver driver = new ChromeDriver(options);
 
         try {
-         
+
             driver.get("file://" + htmlFilePath);
 
             WebDriverWait wait = new WebDriverWait(driver, 10);
             wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".tab")));
 
-            driver.executeScript("var tabs = document.querySelectorAll('.tab'); " +
-                                 "for (var i = 0; i < tabs.length; i++) { " +
-                                 "    tabs[i].click(); " +
-                                 "}");
+            JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+            jsExecutor.executeScript("var tabs = document.querySelectorAll('.tab'); " +
+                                     "for (var i = 0; i < tabs.length; i++) { " +
+                                     "    tabs[i].click(); " +
+                                     "}");
 
             driver.get("chrome://print");
             WebElement printPreviewButton = driver.findElement(By.cssSelector(".action-button"));
@@ -44,19 +44,17 @@ public class HtmlToPdfConverter {
             printButton.click();
             wait.until(ExpectedConditions.urlContains("pdf"));
 
-           
             driver.switchTo().defaultContent();
             driver.get("chrome://downloads");
             wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("downloads-item")));
 
-            driver.executeScript("downloadsManager.setSavePath('" + pdfOutputPath + "');");
+            jsExecutor.executeScript("downloadsManager.setSavePath('" + pdfOutputPath + "');");
             WebElement saveButton = driver.findElement(By.cssSelector("#save"));
             saveButton.click();
             wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("downloads-item")));
         } finally {
-        
+
             driver.quit();
         }
     }
 }
-
